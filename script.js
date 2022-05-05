820e2d340c5a36082dc72b31ee9a2d73f079b983
buhari44 <adeyemibuari@gmail.com>
2022-05-05 13:55:46 +0100

    Developing the homepage

---
+++ script.js
@@ -0,0 +1,32 @@
+// let i=0;
+//  while (i<=20){
+//          console.log("crazy things" + i);
+//          i++
+//  }
+// for( i=10;i<=30;i++){
+//     console.log(i)
+// }
+
+// function read(score){
+//     if(score <= 0){
+//     return -1
+//     }
+//     if(score === 1){
+//         return 1
+//     }
+//     return score + read(score - 1)
+// }
+// console.log(read(3))
+
+for( let ramadan=1; ramadan <= 30; ramadan++){
+
+    // console.log("Day " + ramadan)
+ 
+       if(ramadan > 1){
+ 
+ console.log("Days " + ramadan)
+ 
+               }else{
+                   console.log("Day"+ ramadan)
+               }
+            }
