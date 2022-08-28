79481a49c0644d51dd8fff5ebfc4582ef152a7db
Taiwo Stephen Opeyemi <taiwostephen73@gmail.com>
2022-08-28 15:34:13 +0100

    commit

--- js/members.js
+++ js/members.js
@@ -124,8 +124,7 @@ const search = async()=> {
     `
    }):   searchContainer += `      
    <div class="profile-info">
-          <p id='no-user'>No user with such name.
-          Check the all link icon for all available users.
+          <p id='no-user'>No user with such name.Check the all link icon for all available users.
           </p>
 
     </div>
