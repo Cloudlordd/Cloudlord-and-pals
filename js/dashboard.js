1a3b52c81596f3e67e86c61f675c20c7876b7b51
babboeCodes <babboecodes@gmail.com>
2022-09-15 13:13:05 +0100

    refactor: format code

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,85 +1,74 @@
-const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a')
 const closeIcon = document.querySelector('#close-icon-profile')
 
-
 // TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .fa-bars');
-const sidebar = document.getElementById('sidebar');
-const studentImage = document.getElementById("student-img")
+const menuBar = document.querySelector('#content nav .fa-bars')
+const sidebar = document.getElementById('sidebar')
+const studentImage = document.getElementById('student-img')
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+allSideMenu.forEach((item) => {
+  const li = item.parentElement
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-		closeIcon.classList.add('hide');
-		menuBar.classList.remove('hide');
-	})
-});
-
-
+  item.addEventListener('click', function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove('active')
+    })
+    li.classList.add('active')
+    closeIcon.classList.add('hide')
+    menuBar.classList.remove('hide')
+  })
+})
 
 menuBar.addEventListener('click', function () {
-	menuBar.classList.toggle('hide');
-	closeIcon.classList.toggle('hide');
-	sidebar.classList.toggle('hide');
-	studentImage.classList.toggle('remove')
-	
+  menuBar.classList.toggle('hide')
+  closeIcon.classList.toggle('hide')
+  sidebar.classList.toggle('hide')
+  studentImage.classList.toggle('remove')
 })
 
 closeIcon.addEventListener('click', function () {
-	menuBar.classList.toggle('hide');
-	closeIcon.classList.toggle('hide');
-	sidebar.classList.toggle('hide');
-	studentImage.classList.toggle('remove')
+  menuBar.classList.toggle('hide')
+  closeIcon.classList.toggle('hide')
+  sidebar.classList.toggle('hide')
+  studentImage.classList.toggle('remove')
 })
 
-
-const getImageContainer = document.getElementById("student-img")
-const profileImg = document.querySelector(".profile")
-const socialBox = document.querySelector(".social-box")
-const studentId = document.querySelector(".stuid")
+const getImageContainer = document.getElementById('student-img')
+const profileImg = document.querySelector('.profile')
+const socialBox = document.querySelector('.social-box')
+const studentId = document.querySelector('.stuid')
 
 // get items from local storage
 
 let getData = JSON.parse(localStorage.getItem('data'))
 
-const getProfile = () =>{
-   
-   const {name, track, img, stuId } = getData
-    getImageContainer.innerHTML += `
+const getProfile = () => {
+  const { name, track, img, stuId } = getData
+  getImageContainer.innerHTML += `
 	
 	<img src= ${img} alt="">
 	
 
 	<p class="name">${name}</p>
 	<p class="trac">${track}</p>`
-	
 
-  studentId.innerHTML+=`<p>ID : ${stuId}</p>`
-	profileImg.innerHTML += `
+  studentId.innerHTML += `<p>ID : ${stuId}</p>`
+  profileImg.innerHTML += `
 	<img src= ${img} alt="">
 	`
 
-	return getData;
-	
+  return getData
+}
 
-	}
+getProfile()
 
-	getProfile()
+let container = document.getElementById('container')
+const dashboard = () => {
+  const {
+    socialmedia: { linkedin, github, twitter, portfolio, slackHandle },
+  } = getData
 
-
-
-  let container = document.getElementById("container")
-	const dashboard = () =>{
- 
-
-		const { socialmedia:{linkedin, github, twitter, portfolio,slackHandle} } = getData
-
-container.innerHTML = `
+  container.innerHTML = `
 
 <div class="head-title">
 <div class="left">
@@ -145,19 +134,16 @@ container.innerHTML = `
 
 `
 
-container.classList.remove("container-style")
-
-	}
+  container.classList.remove('container-style')
+}
 
 dashboard()
 console.log(getData)
-const {project} = getData
+const { project } = getData
 console.log(project)
 
-const projects = ()=>{
-
-   container.innerHTML =`
-
+const projects = () => {
+  container.innerHTML = `
    <div class="head-title">
    <div class="left">
    <h1 href="#">Project</h1>
@@ -171,11 +157,11 @@ const projects = ()=>{
    </div>
    
    `
-   const divProjectBox = document.querySelector(".project-box")
-   const projectObject = Object.values({ ...getData.project });
-   projectObject.forEach(item => {
-      const divElement = document.createElement("div");
-      divElement.innerHTML = `
+  const divProjectBox = document.querySelector('.project-box')
+  const projectObject = Object.values({ ...getData.project })
+  projectObject.forEach((item) => {
+    const divElement = document.createElement('div')
+    divElement.innerHTML = `
 	
 						<span class="text">
 							<h3>${item.projectname}</h3>
@@ -185,18 +171,17 @@ const projects = ()=>{
 						<p class="project-description">${item.projectdescription} </p>
 						<a href=${item.projectlink} target =_blank> ${item.projectname}
 						</a>
-	               `;
-      divProjectBox.appendChild(divElement);
-    })
-	
-	container.classList.remove("container-style")
+	               `
+    divProjectBox.appendChild(divElement)
+  })
+
+  container.classList.remove('container-style')
 }
 
+const about = () => {
+  const { intro, introdetails } = getData.description
 
-const about =()=>{
-   const {intro, introdetails}=getData.description
-  
-	container.innerHTML =`
+  container.innerHTML = `
 	
 	<div class="head-title">
 	<div class="left">
@@ -217,51 +202,38 @@ const about =()=>{
 		 </p>
 			</aside>
 	`
-	container.classList.remove("container-style")
-
-	
+  container.classList.remove('container-style')
 }
 
 // all JSON
 let getAllStudent = async () => {
-	let response = await fetch('../studentdata.json') 
-	let result = await response.json() 
-		return result
-   }
-   
-   let output;
-   
-   (async () => {
-   
-	   try {
-   
-		   const res = await getAllStudent()
-		       res.data.students
-		   
-	   } catch (error) {
-		   console.log("Not found", error)
-	   }
-	
-   })()
-   
-   
+  let response = await fetch('../studentdata.json')
+  let result = await response.json()
+  return result
+}
 
-const storeCircle = async () =>{
-	const res = await getAllStudent()
- let response = res.data.students
-	
-    const newArr = response.filter( item=> {
-      
-		 return getData.circle === item.circle
+let output
+;(async () => {
+  try {
+    const res = await getAllStudent()
+    res.data.students
+  } catch (error) {
+    console.log('Not found', error)
+  }
+})()
 
-	})
-	
-	let circleBox = ''
-	newArr.forEach( item =>{
+const storeCircle = async () => {
+  const res = await getAllStudent()
+  let response = res.data.students
 
+  const newArr = response.filter((item) => {
+    return getData.circle === item.circle
+  })
 
-		const {name, img, track} =item
-  circleBox +=`
+  let circleBox = ''
+  newArr.forEach((item) => {
+    const { name, img, track } = item
+    circleBox += `
       <div class="circle-box">
         <img src= ${img}>
   
@@ -269,31 +241,23 @@ const storeCircle = async () =>{
 
 		<p class ="track"> ${track}</p>
 	  </div>
-  `  
+  `
+  })
 
+  container.innerHTML = circleBox
+  container.classList.add('container-style')
+}
 
-	})
+container.classList.remove('container-style')
 
-	container.innerHTML = circleBox
-  container.classList.add("container-style")
-	
-  
-  }
-  
-  container.classList.remove("container-style")
-  
 // all tabs .js
-
-
-
 
 // logout
 
-const logout = ()=>{
+const logout = () => {
+  container.classList.remove('container-style')
 
-	container.classList.remove("container-style");
-	
- container.innerHTML = `
+  container.innerHTML = `
 	 <div class = "parent-logout">
 
 	<div>
@@ -307,21 +271,20 @@ const logout = ()=>{
 		 </div>
 	</div>
  `
-
 }
 
-// function to close side bar when any of the side bar menu is clicked 
-const [SIDEBAR_LIST1, SIDEBAR_LIST2] = document.querySelectorAll('.side-menu');
-const SIDEBAR_ITEM1 = SIDEBAR_LIST1.querySelectorAll(':scope > li');
-const SIDEBAR_ITEM2 = SIDEBAR_LIST2.querySelectorAll(':scope > li');
-const sidebarList = [...SIDEBAR_ITEM1, ...SIDEBAR_ITEM2];
+// function to close side bar when any of the side bar menu is clicked
+const [SIDEBAR_LIST1, SIDEBAR_LIST2] = document.querySelectorAll('.side-menu')
+const SIDEBAR_ITEM1 = SIDEBAR_LIST1.querySelectorAll(':scope > li')
+const SIDEBAR_ITEM2 = SIDEBAR_LIST2.querySelectorAll(':scope > li')
+const sidebarList = [...SIDEBAR_ITEM1, ...SIDEBAR_ITEM2]
 
 const sidebarClickHandler = () => {
-   for (const item of sidebarList) {
-      item.addEventListener('click', () => {
-         sidebar.classList.add('hide');
-			studentImage.classList.add('remove');
-      } )
-   }
+  for (const item of sidebarList) {
+    item.addEventListener('click', () => {
+      sidebar.classList.add('hide')
+      studentImage.classList.add('remove')
+    })
+  }
 }
-sidebarClickHandler();
+sidebarClickHandler()
